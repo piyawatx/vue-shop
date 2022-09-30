7a411fba01d15434acefaab2435471c6ac8cb403
piyawat <piyawat>
2022-09-30 12:29:14 +0700

    แก้ navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,26 +1,28 @@
 <template>
   <div class="container-fluid bg-dark text-white">
     <div class="container px-0 pb-3">
-      <div class="">
+      <div class="mb-1">
         <ul class="nav justify-content-end">
           <li class="nav-item">
-            <a class="nav-link link-light" href="#">สมัครสมาชิก</a>
+            <a class="nav-link link-light pe-0" href="#">สมัครสมาชิก</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link link-light" href="#">เข้าสู่ระบบ</a>
+            <a class="nav-link link-light pe-0" href="#">เข้าสู่ระบบ</a>
           </li>
         </ul>
       </div>
       <div class="d-flex">
         <div class="d-none d-sm-block me-5">
           <a class="navbar-brand d-flex" href="#">
-          <img src="../assets/logo.png" width="40" class="align-self-center" />
-          <span class="fs-3 ms-1 align-self-center">
-            Vue Shop
-          </span>
-        </a>
+            <img
+              src="../assets/logo.png"
+              width="40"
+              class="align-self-center"
+            />
+            <span class="fs-3 ms-1 align-self-center"> Vue Shop </span>
+          </a>
         </div>
-        
+
         <div class="container-fluid align-self-center p-0 me-4">
           <div class="input-group">
             <input type="text" class="form-control" placeholder="ค้นหาสินค้า" />
