02ecbbbe8f02cd9f9485c76ff48aa4876b5cc384
piyawat <piyawat>
2022-10-07 12:47:03 +0700

    category cursor

--- src/components/ProductTab.vue
+++ src/components/ProductTab.vue
@@ -6,10 +6,12 @@
     </div>
     <hr class="text-muted" />
     <div>
-      <div @click="selectCategory()">สินค้าทั้งหมด</div>
-      <div @click="selectCategory(1)">เสื้อ</div>
-      <div @click="selectCategory(2)">กางเกง</div>
-      <div @click="selectCategory(3)">รองเท้า</div>
+      <div class="user-select-none" @click="selectCategory()">
+        สินค้าทั้งหมด
+      </div>
+      <div class="user-select-none" @click="selectCategory(1)">เสื้อ</div>
+      <div class="user-select-none" @click="selectCategory(2)">กางเกง</div>
+      <div class="user-select-none" @click="selectCategory(3)">รองเท้า</div>
     </div>
   </div>
 </template>
