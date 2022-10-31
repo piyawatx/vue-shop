932532f6dc671217a221f00a92cf3f955443e65c
piyawatx <81255207+piyawatx@users.noreply.github.com>
2022-10-31 13:32:57 +0700

    จัดหน้า ProductView

--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -1,28 +1,31 @@
 <template>
   <div class="container" v-if="product.title">
-    <!-- <h1>Product</h1>
-    {{ $route.params.id }} -->
-
-    <div class="card card-body mt-3 border-0 shadow-sm">
+    <div class="card card-body mt-3 border-0 shadow-sm p-3">
       <div class="d-md-flex d-grid gap-3">
         <div class="image text-center">
           <img :src="product.imageUrl" alt="" />
         </div>
-        <div>
-          <div class="title mb-2 fs-4 lh-1">{{ product.title }}</div>
-          <div class="price mb-2 fs-2">฿{{ product.price.toLocaleString() }}</div>
-          <div class="stock mb-2 text-muted">
-            มีสินค้าทั้งหมด {{ product.stock }} ชิ้น
+        <div class="d-flex flex-column">
+          <div>
+            <div class="title mb-2 fs-4 lh-1">{{ product.title }}</div>
+            <div class="price mb-2 fs-2">
+              ฿{{ product.price.toLocaleString() }}
+            </div>
           </div>
           <div>
-            <button
-              type="button"
-              class="btn btn-outline-dark me-3"
-              @click="addCart(product)"
-            >
-              เพิ่มไปยังรถเข็น
-            </button>
-            <button type="button" class="btn btn-dark">ซื้อสินค้า</button>
+            <div class="stock mb-3 text-muted">
+              มีสินค้าทั้งหมด {{ product.stock }} ชิ้น
+            </div>
+            <div>
+              <button
+                type="button"
+                class="btn btn-outline-dark me-3"
+                @click="addCart(product)"
+              >
+                เพิ่มไปยังรถเข็น
+              </button>
+              <button type="button" class="btn btn-dark">ซื้อสินค้า</button>
+            </div>
           </div>
         </div>
       </div>
@@ -81,5 +84,4 @@ img {
 .title {
   font-size: 1em;
 }
-
 </style>
