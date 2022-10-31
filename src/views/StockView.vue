72fc3dbd22ce89ed1cb7f3d656582356506ee03f
piyawatx <81255207+piyawatx@users.noreply.github.com>
2022-10-31 11:57:12 +0700

    delete Product

--- src/views/StockView.vue
+++ src/views/StockView.vue
@@ -17,7 +17,9 @@
               <th style="min-width: 300px">Title</th>
               <th style="width: 200px; min-width: 100px">Price</th>
               <th>Stock</th>
-              <th class="text-center" style="width: 130px; min-width: 120px">Actions</th>
+              <th class="text-center" style="width: 130px; min-width: 120px">
+                Actions
+              </th>
             </tr>
           </thead>
           <tbody>
@@ -34,7 +36,11 @@
                   <button type="button" class="btn btn-sm btn-primary me-2">
                     Edit
                   </button>
-                  <button type="button" class="btn btn-sm btn-danger">
+                  <button
+                    type="button"
+                    class="btn btn-danger btn-sm"
+                    @click="confirmDelete(product.id)"
+                  >
                     Delete
                   </button>
                 </div>
@@ -48,6 +54,8 @@
 </template>
 
 <script>
+import axios from 'axios'
+import Swal from 'sweetalert2'
 export default {
   created() {
     this.$store.dispatch("fetchProducts");
@@ -55,6 +63,37 @@ export default {
   methods: {
     addProduct() {
       this.$router.push("/stock/add");
+    },
+    confirmDelete(id) {
+      Swal.fire({
+        title: 'Are you sure?',
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Delete',
+      }).then((result) => {
+        if (result.isConfirmed) {
+          this.deleteProduct(id)
+        }
+      })
+    },
+    async deleteProduct(id) {
+      await axios
+        .delete(
+          'https://vue-shop-5f2cd-default-rtdb.asia-southeast1.firebasedatabase.app/products/' +
+            id +
+            '.json'
+        )
+        .then(() => {
+          this.$store.dispatch('fetchProducts')
+          Swal.fire({
+            icon: 'success',
+            title: 'Deleted!',
+            showConfirmButton: false,
+            timer: 1500,
+          })
+        })
     },
   },
 };
